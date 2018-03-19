398f9947e06596c39b9fcd2a7c02fc8a9bb31b76
Felipe Rosa Renck <felipe-renck@hotmail.com>
2018-03-19 00:55:07 -0300

    Add Eventos Page

--- src/app/janovohamburgo/coralnh-sistema/sistema/eventos/eventos.component.ts
+++ src/app/janovohamburgo/coralnh-sistema/sistema/eventos/eventos.component.ts
@@ -1,4 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { MatDialog, MatDialogRef, MatSnackBar } from '@angular/material';
+import { User } from 'app/models/User';
+import { FormControl, Validators, FormBuilder } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Http, Headers } from '@angular/http';
+import { UserService } from 'app/services/user.service';
+import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatDatepickerModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDialogModule,
+  MatFormField,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+} from '@angular/material';
 
 @Component({
   selector: 'app-eventos',
@@ -7,9 +52,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  constructor() { }
+  public values = [
+    { value: 1, text: 'Carro' },
+    { value: 2, text: 'Ônibus' }
+  ];
+
+  public nome;
+  public automovel;
+  public hideEvento = true;
+
+
+  constructor() {
+  }
 
   ngOnInit() {
+
   }
+
+  showEvento = function () {
+    this.hideEvento = !this.hideEvento;
+    console.log(this.hideEvento);
+}
 
 }
